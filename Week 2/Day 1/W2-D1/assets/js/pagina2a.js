56b461ad69c81ad7c99fc38df9c4617001e91172
manulelee <emanuele.sy@gmail.com>
2023-02-06 15:48:50 +0100

    javascript giorno 1

---
+++ Week 2/Day 1/W2-D1/assets/js/pagina2a.js
@@ -0,0 +1,2 @@
+document.getElementById("h1").innerHTML = "Iniziamo la nostra pratica";
+document.getElementById("firstP").innerHTML = "output scritto da JavaScript";
---
+++ Week 2/Day 1/W2-D1/assets/js/pagina2b.js
@@ -0,0 +1,16 @@
+alert("Usa il pop up per dare dei messaggi");
+window.alert("Sto studiando JS");
+
+var animale = "rinoceronte";
+var animaleDue = "zanzara";
+var AnimaleTre = "tigre";
+var animale_4 = "elefante";
+
+var author = "scritto da JavaScript";
+
+console.log(animale);
+console.log(animaleDue);
+console.log(AnimaleTre);
+console.log(animale_4);
+
+document.write(`<p>${animaleDue} ${AnimaleTre} ${author}</p>`);
