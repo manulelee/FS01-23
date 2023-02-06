1af781ddcc224cd2ca17702ce04c7a2ba1eaf0e1
manulelee <emanuele.sy@gmail.com>
2023-02-06 16:28:41 +0100

    correzioni

--- Week 2/Day 1/W2-D1/assets/js/pagina2a.js
+++ Week 2/Day 1/W2-D1/assets/js/pagina2a.js
@@ -1,2 +1,5 @@
+alert("Usa il pop up per dare dei messaggi");
+window.alert("Sto studiando JS");
+
 document.getElementById("h1").innerHTML = "Iniziamo la nostra pratica";
 document.getElementById("firstP").innerHTML = "output scritto da JavaScript";
