d153f13abee5dc17a7f644cc13b1a800940443ee
manulelee <emanuele.sy@gmail.com>
2023-02-17 00:38:37 +0100

    da rivedere 16 e 18 + HTML & CSS

--- Week 3/Day 2/W3 - D2/assets/js/D7.js
+++ Week 3/Day 2/W3 - D2/assets/js/D7.js
@@ -2,65 +2,187 @@
   Scrivi una funzione per concatenare due stringhe ricevute come parametri, selezionando solamente i primi 2 caratteri della
   prima e gli ultimi 3 della seconda. Converti la stringa risultante in maiuscolo e mostrala con un console.log().
 */
-var stringa1 = "ciao ciao ciao";
-var stringa2 = "hello hello hello";
 
-nuovaStringa1 = stringa1.slice(0, 2);
-nuovaStringa2 = stringa2.slice(-3);
-var stringa3 = nuovaStringa1.concat(nuovaStringa2);
-console.log(stringa1);
-console.log(stringa2);
-console.log(stringa3);
+console.log("Esercizio 1");
+
+primaFunzione = (_stringa1, _stringa2) => {
+  var result = _stringa1.slice(0, 2).concat(_stringa2.slice(-3));
+  return console.log(result);
+};
+
+primaFunzione("ciao", "hello");
 
 /* ESERCIZIO 2
   Scrivi una funzione per creare un array di soli valori DISPARI da 1 a 100.
  */
 
-/*function numDisp(){
+console.log("Esercizio 2");
+
+secondaFunzione = (_n) => {
   var array = [];
-  for (i = 0; i < array.length; i++) {
-  array.push((Math.floor(Math.random)*100)+1); 
-}
-return array;
-}
-console.log(array);*/
+  while (array.length < _n) {
+    randomNum = Math.floor(Math.random() * 100) + 1;
+    //console.log(randomNum);
+    if (randomNum % 2 != 0) {
+      array.push(randomNum);
+    }
+  }
+  return console.log(array);
+};
+
+secondaFunzione(3);
+
 /* ESERCIZIO 3
   Scrivi una funzione per creare un array di soli valori multipli di 5, da 1 a 100.
  */
+
+console.log("Esercizio 3");
+
+terzaFunzione = () => {
+  var array = [];
+  for (let i = 1; i <= 100; i++) {
+    if (i % 5 == 0) {
+      array.push(i);
+    }
+  }
+  return console.log(array);
+};
+
+terzaFunzione();
 
 /* ESERCIZIO 4
   Scrivi una funzione per creare un array di 10 elementi; ognuno di essi deve essere un valore random compreso tra 0 e 100 (incluso).
  */
 
+console.log("Esercizio 4");
+
+quartaFunzione = () => {
+  var array = [];
+  for (let i = 0; i < 10; i++) {
+    randomNum = Math.floor(Math.random() * 101);
+    array.push(randomNum);
+  }
+  return console.log(array);
+};
+
+quartaFunzione();
+
 /* ESERCIZIO 5
   Scrivi una funzione per ricavare solamente i valori PARI da un array composto da soli valori numerici.
  */
+
+console.log("Esercizio 5");
+
+quintaFunzione = (_array) => {
+  var result = [];
+  _array.forEach((element) => {
+    if (parseInt(element) % 2 == 0) {
+      result.push(parseInt(element));
+    }
+  });
+  return console.log(result);
+};
+
+quintaFunzione([3, 4, 7, 6, 5, "2"]);
 
 /* ESERCIZIO 6
   Scrivi una funzione per sommare a catena i numeri contenuti in un array.
  */
 
+console.log("Esercizio 6");
+
+sestaFunzione = (_array) => {
+  var result = "";
+  for (let i = 0; i < _array.length; i++) {
+    result += _array[i];
+  }
+  return console.log(result);
+};
+
+sestaFunzione([7, 6, 8, 9, 11]);
+
 /* ESERCIZIO 7
   Scrivi una funzione per incrementare di 1 tutti i valori numerici in un array.
 */
+
+console.log("Esercizio 7");
+
+settimaFunzione = (_array) => {
+  for (let i = 0; i < _array.length; i++) {
+    _array[i]++;
+  }
+  return console.log(_array);
+};
+
+settimaFunzione([1, 2, 3, 4, 5]);
 
 /* ESERCIZIO 8
   Sostituisci ogni stringa contenuta in un array con un numero rappresentante la sua lunghezza.
   es.: ["EPICODE", "is", "great"] => [7, 2, 5]
 */
 
+console.log("Esercizio 8");
+
+ottavaFunzione = (_array) => {
+  for (let i = 0; i < _array.length; i++) {
+    _array[i] = _array[i].length;
+  }
+  return console.log(_array);
+};
+
+ottavaFunzione(["EPICODE", "is", "great"]);
+
 /* ESERCIZIO 9 (EXTRA)
   Scrivi una funzione per eliminare solo i valori PARI da un array.
 */
 
+console.log("Esercizio 9");
+
+nonaFunzione = (_array) => {
+  for (let i = 0; i < _array.length; i++) {
+    if (_array[i] % 2 == 0) {
+      _array.splice(i, 1);
+    }
+  }
+  return console.log(_array);
+};
+
+nonaFunzione([3, 4, 7, 6, 5]);
+
 /* ESERCIZIO 10 (EXTRA)
   Scrivi una funzione per creare un array di 10 elementi; ognuno di essi deve essere un valore random compreso tra 0 e 10 (incluso), SENZA AMMETTERE DUPLICATI.
  */
+console.log("Esercizio 10");
+
+decimaFunzione = () => {
+  var array = [];
+  while (array.length < 10) {
+    randomNum = Math.floor(Math.random() * 11);
+    if (array.includes(randomNum)) {
+      //console.log(`Il numero ${randomNum} è gia presente nell'array.`);
+    } else {
+      array.push(randomNum);
+    }
+  }
+  return console.log(array);
+};
+
+decimaFunzione();
 
 /* ESERCIZIO 11 (EXTRA)
   Scrivi un algoritmo in grado di invertire un array.
   es: [1, 3, 5] ==> [5, 3, 1]
 */
+
+console.log("Esercizio 11");
+
+undicesimaFunzione = (_array) => {
+  console.log(`array prima: ${_array}`);
+  _array.reverse();
+  return console.log(`array dopo: ${_array}`);
+};
+
+undicesimaFunzione([1, 3, 5]);
 
 /* Questo array di film verrà usato negli esercizi a seguire. Non modificarlo e scorri oltre per riprendere gli esercizi :) */
 const movies = [
@@ -177,17 +299,65 @@ const movies = [
   Scrivi una funzione per trovare il film più vecchio nell'array fornito.
 */
 
+console.log("Esercizio 12");
+
+dodicesimaFunzione = () => {
+  var oldest = movies[0];
+  movies.forEach((element) => {
+    if (element.Year < oldest.Year) {
+      oldest = element;
+    }
+  });
+  return console.log(`Il film più vecchio è: ${oldest.Title} (${oldest.Year})`);
+};
+
+dodicesimaFunzione();
+
 /* ESERCIZIO 13
   Scrivi una funzione per ottenere il numero di film contenuti nell'array fornito.
 */
+
+console.log("Esercizio 13");
+
+tredicesimaFunzione = () => {
+  return console.log(`Sono presenti ${movies.length} film`);
+};
+
+tredicesimaFunzione();
 
 /* ESERCIZIO 14
   Scrivi una funzione per creare un array con solamente i titoli dei film contenuti nell'array fornito.
 */
 
+console.log("Esercizio 14");
+
+quattordicesimaFunzione = () => {
+  var titoli = [];
+  movies.forEach((element) => {
+    titoli.push(element.Title);
+  });
+  return console.log(titoli);
+};
+
+quattordicesimaFunzione();
+
 /* ESERCIZIO 15
   Scrivi una funzione per ottenere dall'array fornito solamente i film usciti nel millennio corrente.
 */
+
+console.log("Esercizio 15");
+
+quindicesimaFunzione = () => {
+  var titoli = [];
+  movies.forEach((element) => {
+    if (element.Year >= 2000) {
+      titoli.push(element.Title);
+    }
+  });
+  return console.log(`Film usciti dopo il 2000: ${titoli}`);
+};
+
+quindicesimaFunzione();
 
 /* ESERCIZIO 16
   Scrivi una funzione per ottenere dall'array fornito uno specifico film (la funzione riceve un imdbID come parametro).
@@ -202,6 +372,8 @@ window.addEventListener("load", function () {
 
 btnRicerca.addEventListener("click", function () {
   let ricerca = document.getElementById("selectFilm").value;
+  let container = document.getElementById("container");
+  container.style.backgroundColor = "white";
   if (ricerca == "") {
     document.getElementById("title").innerHTML = "scegli almeno un film";
     document.getElementById("Year").innerHTML = "";
@@ -232,6 +404,18 @@ function trova(ricerca) {
 /* ESERCIZIO 17
   Scrivi una funzione per calcolare la somma di tutti gli anni in cui sono stati prodotti i film contenuti nell'array fornito.
 */
+
+console.log("Esercizio 17");
+
+diciasettesimaFunzione = () => {
+  let somma = 0;
+  movies.forEach((element) => {
+    somma += parseInt(element.Year);
+  });
+  return console.log(`La somma degli anni è: ${somma}`);
+};
+
+diciasettesimaFunzione();
 
 /* ESERCIZIO 18 (EXTRA)
   Scrivi una funzione per recuperare tutti i film dall'array fornito che contengono una parola fornita.
