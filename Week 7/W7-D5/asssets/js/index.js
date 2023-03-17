e25bdcf0953e24af2a4ae84e32d8c9c7dafc090c
manulelee <emanuele.sy@gmail.com>
2023-03-17 17:06:53 +0100

    fine?

--- Week 7/W7-D5/asssets/js/index.js
+++ Week 7/W7-D5/asssets/js/index.js
@@ -94,20 +94,3 @@ const search = function () {
     console.log(card.innerText.includes(searchQuery.value)); //.includes(searchQuery.value)
   });
 };
-
-function myFunction() {
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.querySelector("nav form");
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName("li");
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}
